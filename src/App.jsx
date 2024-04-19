1331b8a5acd8c75901665d50f55903e255690cf2
phamninh <phamninh708@gmail.com>
2024-04-19 17:35:20 +0700

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import { ThemeProvider } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import { useState } from 'react'
+
+export function SelectSmall() {
+  const [mode, setMode] = useState('light')
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">Light</MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+        <MenuItem value="system">System</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 
 function App() {
 
+
   return (
     <>
+      <SelectSmall />
     </>
   )
 }
