bbcc2cc5be13b69841740aaccf9527f32c89426d
Pham Ninh <Pham Ninh@DESKTOP-B136F29>
2024-05-08 22:20:04 +0700

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from './theme.jsx'
 import './index.css'
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import { ToastContainer, Zoom } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
